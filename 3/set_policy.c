862a1eeb3c92c4f241f735bb11881d51a025adfc
root <boaz30333@gmail.com>
2020-08-07 18:49:01 +0300

    second

--- 3/set_policy.c
+++ 3/set_policy.c
@@ -1,37 +1,83 @@
+#define _GNU_SOURCE
 #include <signal.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
  #include <sched.h>
 #include <linux/sched.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <linux/unistd.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <sys/syscall.h>
+#include <pthread.h>
+
+
+struct sched_attr
+{
+    __u32 size;
+
+    __u32 sched_policy;
+    __u64 sched_flags;
+
+    /* SCHED_NORMAL, SCHED_BATCH */
+    __s32 sched_nice;
+
+    /* SCHED_FIFO, SCHED_RR */
+    __u32 sched_priority;
+
+   /* SCHED_DEADLINE (nsec) */
+   __u64 sched_runtime;
+   __u64 sched_deadline;
+   __u64 sched_period;
+};
+
+int sched_setattr(pid_t pid,
+         const struct sched_attr *attr,
+         unsigned int flags)
+{
+   return syscall(__NR_sched_setattr, pid, attr, flags);
+}
+
+int sched_getattr(pid_t pid,
+          struct sched_attr *attr,
+          unsigned int size,
+          unsigned int flags){
+    return syscall(__NR_sched_getattr, pid, attr, size, flags);
+}
+
 int main(int argc, char *argv[])
 {
-    int policy;
-    int priority;
-struct sched_param sp;
-	struct sched_attr attr1 = {
-		.size = sizeof(struct sched_attr),
+    int policy=atoi(argv[1]);;
+    int priority=atoi(argv[2]);
+struct sched_param s_param;
+	struct sched_attr s_attr;
+    if(policy==6){
+      		s_attr.size = sizeof(struct sched_attr);
 
-		.sched_policy = SCHED_DEADLINE,
-		.sched_flags = 0,
+		s_attr.sched_policy = SCHED_DEADLINE;
+		s_attr.sched_flags = 0;
 
-		.sched_nice = 0,
+		s_attr.sched_nice = 0;
 
-		.sched_priority = 0,
+		s_attr.sched_priority = priority;
 
-		.sched_runtime =   100 * 1000,
-		.sched_period =   1000 * 1000,
-		.sched_deadline = 1000 * 1000,
-	};
-    policy = atoi(argv[1]);
-    priority = atoi(argv[2]);
-    if(policy==SCHED_DEADLINE)
-sp.sched_priority= priority;
-
- 
-    if (sched_setscheduler(0, policy, &sp) == -1)
-        errExit("sched_setscheduler");
-
+		s_attr.sched_runtime =   100 * 1000;
+		s_attr.sched_period =   1000 * 1000;
+		s_attr.sched_deadline = 1000 * 1000;
+        if(sched_setattr(0,&s_attr,0) == -1){
+         perror("sched_setattr");
+         exit(EXIT_FAILURE);
+    }
+    }
+    else{
+s_param.sched_priority= priority;
+    if (sched_setscheduler(0, policy, &s_param) == -1){
+         perror("sched_setscheduler");
+         exit(EXIT_FAILURE);   } }
 while(1){
 
 }
